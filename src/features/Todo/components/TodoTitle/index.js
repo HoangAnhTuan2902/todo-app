da321c8fdbb5212187c518b0f2a6d591671ff420
HoangAnhTuan2902 <hoanganhtuan29022004@gmail.com>
2024-07-06 10:04:28 +0700

    Refactor TodoTitle component to update project name in the title

--- src/features/Todo/components/TodoTitle/index.js
+++ src/features/Todo/components/TodoTitle/index.js
@@ -2,7 +2,7 @@ function TodoTitle() {
 	return (
 		<div className="page-header">
 			<h1>
-				Project ToDo List App<small>ReactJS</small>
+				Project ToDo List App <small>ReactJS</small>
 			</h1>
 		</div>
 	);
